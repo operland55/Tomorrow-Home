988335abdededc3eadab81d67a49a7c3d9f397dd
jungpa <operland55@naver.com>
2022-08-24 12:13:08 +0900

    🚧 Implement showing full product-shipment section

---
+++ js/section-header.js
@@ -0,0 +1,10 @@
+const productShipment = document.querySelector('.product-shipment');
+const sectionHeaderIconButton = document.querySelector('.product-shipment .product-section-header.sm-only .icon-button');
+
+
+function showFullSection() {
+  const section = this.parentNode.parentNode;
+  section.classList.add('is-open');
+}
+
+sectionHeaderIconButton.addEventListener('click', showFullSection)
