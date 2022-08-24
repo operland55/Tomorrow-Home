bd2b0e0d368e19f00525cc231c6d3ae462dd650b
jungpa <operland55@naver.com>
2022-08-24 15:52:14 +0900

    🚧 Implement toggling bookmark-button

--- js/order-cta.js
+++ js/order-cta.js
@@ -1,6 +1,6 @@
 const orderCta = document.querySelector('.order-cta');
 const [orderCtaBookmarkButton, orderCtaBuyButton] = orderCta.children;
-
+let value = true;
 
 const orderModal = document.querySelector('.order-form-modal');
 const orderModalOverlay = document.querySelector('.overlay');
@@ -17,3 +17,27 @@ function closeOrderModal() {
 
 orderCtaBuyButton.addEventListener('click', openOrderModal)
 orderModalOverlay.addEventListener('click', closeOrderModal)
+
+function toggleOrderCtaBookmark(e) {
+  const [icon, countSpan] = this.children;  //[아이콘,span]
+  let count = Number(countSpan.innerHTML.replace(',', ''))
+  let newCount = count;
+
+  if (this.classList.contains('is-active')) {
+    // Note :활성화가 된 상태이니 -> 비활성화 (ic-bookmark)
+    icon.classList.add('ic-bookmark')
+    icon.classList.remove('ic-bookmark-filled')
+    newCount = newCount - 1;
+  } else {
+    // Note :비활성화가 된 상태이니 -> 활성화 (ic-bookmark-filled)
+    icon.classList.add('ic-bookmark-filled')
+    icon.classList.remove('ic-bookmark')
+    newCount = newCount + 1;
+  }
+  countSpan.innerHTML = newCount.toLocaleString();
+  this.classList.toggle('is-active');
+}
+
+orderCtaBookmarkButton.addEventListener('click', toggleOrderCtaBookmark)
+
+
