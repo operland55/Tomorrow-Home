c006740fa94b6668631e08972cb43d1a83c13c54
jungpa <operland55@naver.com>
2022-08-25 11:28:34 +0900

    🚧 Implement toggling my-menu

---
+++ js/my-menu.js
@@ -0,0 +1,22 @@
+const myMenu = document.querySelector('.my-menu')
+const myMenuButton = document.querySelector('.my-menu-button')
+
+function toggleMenu() {
+  if (myMenu.classList.contains('is-active')) {
+    window.addEventListener('click', closeMyMenuOnClickingOutside)
+
+  }
+  myMenu.classList.toggle('is-active')
+}
+
+myMenuButton.addEventListener('click', toggleMenu)
+
+
+function closeMyMenuOnClickingOutside(e) {
+  if (!myMenu.contains(e.target)) {
+    myMenu.classList.remove('is-active')
+    window.removeEventListener('click', closeMyMenuOnClickingOutside)
+
+  }
+
+}
