a1049b56669dc48a5f5b83dab6037d4b100c322f
jungpa <operland55@naver.com>
2022-08-25 20:28:35 +0900

    🚧 Implement deleting individual search-history-item

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -4,6 +4,9 @@ const gnbSearchHistory = gnbSearch.querySelector('.search-history')
 const gnbSearchHistoryList = gnbSearchHistory.querySelector('ol')
 
 const deleteAllButton = gnbSearchHistory.querySelector('.search-history-header button');
+const deleteButtonList = gnbSearchHistoryList.querySelectorAll('li .delete-button');
+
+
 
 function openGnbSearchHistory() {
   if (gnbSearchHistoryList.children.length == 0) return; // list 자식이0개라면 기록창 열리지않게하는법
@@ -28,6 +31,24 @@ function deleteAllSearchHistories() {
   gnbSearchHistoryList.innerHTML = '';
   gnbSearchHistory.classList.remove('is-active')
 }
+
+// 하나만 지우는  삭제클릭
+
+function deleteSearchHistoryItem(e) {
+  e.stopPropagation();  // 이벤트 전파를 막을 수 있다.
+  const itemToDelete = this.parentNode;
+  gnbSearchHistoryList.removeChild(itemToDelete);
+
+  if (gnbSearchHistoryList.children.length == 0) {
+    gnbSearchHistory.classList.remove('is-active')
+  }
+}
+
+deleteButtonList.forEach(i => {
+  i.addEventListener('click', deleteSearchHistoryItem)
+})
+
+deleteButton.addEventListener('click', deleteSearchHistoryItem)
 
 
 deleteAllButton.addEventListener('click', deleteAllSearchHistories)
